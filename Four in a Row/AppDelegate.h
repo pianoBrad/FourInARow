c1e0b92e56605876364c487c8e44488d97ea287a
Brad Phillips <brad@bradphillips.org>
2015-05-16 17:27:03 -0500

    Initial Commit

---
+++ Four in a Row/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Four in a Row
+//
+//  Created by Brad Phillips on 5/16/15.
+//  Copyright (c) 2015 bradphillips. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Four in a Row/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Four in a Row
+//
+//  Created by Brad Phillips on 5/16/15.
+//  Copyright (c) 2015 bradphillips. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
